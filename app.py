9c8f59e749a2bfaf398eff1954ae17790c043050
Joshua Ashkinaze <josh.ashkinaze@gmail.com>
2023-12-25 08:29:50 -0500

    working but manic

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from openai import OpenAI
 import json
 import string
 import random
+import re
 
 app = Flask(__name__)
 
@@ -11,7 +12,7 @@ with open('secrets.json') as f:
     secrets = json.load(f)
 openai_key = secrets['openai_key']
 
-def get_chat_completion(prompt, model="gpt-3.5-turbo", attempt_no=0, max_attempts=2):
+def get_chat_completion(context, incomplete_sentence, model="gpt-3.5-turbo", attempt_no=0, max_attempts=2):
   if attempt_no > max_attempts:
     return None
   else:
@@ -22,28 +23,22 @@ def get_chat_completion(prompt, model="gpt-3.5-turbo", attempt_no=0, max_attempt
         messages=[
           {
             "role": "system",
-              "content": "Your task is to project and complete the user's description of a day in their life into a scenario set in a world affected by unchecked climate change. "
-                         "Draw on realistic consequences of environmental changes, such as extreme weather, resource scarcity, and ecological impacts. "
-                         "Adapt the user's narrative to reflect these changes, maintaining the essence of their original story but altering the setting and events to align with the effects of climate change. "
-                         "Ensure that the completion is coherent with the user's text and provides a vivid, believable depiction of life in this altered world. "
-                         "Write in the first person, from the user's perspective. "
-                         "Do not repeat yourself ever."
-
+              "content": f"INSTRUCTIONS: You will autocomplete a user's sentence, writing in the first person. I will provide you with CONTEXT and an INCOMPLETE SENTENCE and you will return the COMPLETED SENTENCE that is consistent with CONTEXT. Write in the first person."
           },
           {
             "role": "user",
-            "content": prompt
+            "content": f"CONTEXT:{context}\nINCOMPLETE SENTENCE:{incomplete_sentence}"
           }
         ],
-        temperature=random.uniform(0.1, 0.4),
-        max_tokens=random.randint(5, 15),
+        temperature=random.uniform(0.8, 0.9),
+        max_tokens=random.randint(10, 15),
         top_p=1
       )
       answer = json.loads(response.choices[0].json())['message']['content']
       return answer
     except Exception as e:
       print(e)
-      return get_chat_completion(prompt, attempt_no=attempt_no+1)
+      return get_chat_completion(context, incomplete_sentence, model="gpt-3.5-turbo", attempt_no=attempt_no+1, max_attempts=2)
 
 @app.route('/')
 def index():
@@ -73,13 +68,69 @@ def autocomplete():
     """ Handle the autocomplete request. """
     text = request.json.get('text')
     sanitized_text = sanitize_text(text)
-    completion = get_chat_completion(sanitized_text)
-    full_word_completion = extract_complete_words(completion)
+    context, incomplete_sentence = get_context_and_incomplete_sentence(sanitized_text)
+    completion = get_chat_completion(context=context, incomplete_sentence=incomplete_sentence)
+    full_word_completion = remove_duplicated_completion(context, completion)
     print("TEXT:", text)
+    print("CONTEXT:", context)
+    print("INCOMPLETE SENTENCE:", incomplete_sentence)
+
     print("COMPLETION:", completion)
     print("FULL WORD COMPLETION:", full_word_completion)
-    return jsonify(completion=full_word_completion)
+    print("REMOVE DUPLICATED COMPLETION:", remove_duplicated_completion(context, completion))
+    return jsonify(completion=remove_duplicated_completion(incomplete_sentence, completion))
 
+def remove_duplicated_completion(text, completion):
+    """
+    Removes the duplicated part from the beginning of the completion
+    if it overlaps with the end of the text.
+    """
+    if not text or not completion:
+        return completion
+
+    text_words = text.split()
+    completion_words = completion.split()
+
+    # Find the overlapping length
+    overlap_len = 0
+    for i in range(1, min(len(text_words), len(completion_words)) + 1):
+        if text_words[-i:] == completion_words[:i]:
+            overlap_len = i
+
+    # Remove the overlapping part from the completion
+    return ' '.join(completion_words[overlap_len:])
+
+
+def get_context_and_incomplete_sentence(text):
+    """
+    Extracts context and incomplete sentence from the given text.
+    Further adjusted to correctly identify incomplete sentences at the end.
+    """
+    sentence_terminators = r"[.!?]"
+
+    # Split the text using the sentence terminators and keep the terminators
+    sentences = re.split('({})'.format(sentence_terminators), text)
+    # Reassemble sentences with their terminators
+    sentences = [sentences[i] + (sentences[i+1] if i+1 < len(sentences) else '') for i in range(0, len(sentences), 2)]
+
+    if len(sentences) == 1:
+        # Single sentence which could be complete or incomplete
+        if text.endswith(tuple(sentence_terminators)):
+            return sentences[0].strip(), ''
+        else:
+            return '', sentences[0].strip()
+    else:
+        # Multiple sentences
+        if text.endswith(tuple(sentence_terminators)):
+            # Text ends with a full stop, question mark, or exclamation mark
+            context = ' '.join(sentences)
+            incomplete_sentence = ""
+        else:
+            # Text ends with an incomplete sentence
+            context = ' '.join(sentences[:-1])
+            incomplete_sentence = sentences[-1]
+
+    return context.strip(), incomplete_sentence.strip()
 
 def is_partial_word(sentence):
     """ Check if the sentence ends with a partial word. """
