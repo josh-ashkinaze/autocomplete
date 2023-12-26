1437e0d0614ebea5af36469240b787ed845fa2b4
Joshua Ashkinaze <josh.ashkinaze@gmail.com>
2023-12-26 09:48:19 -0500

    fixed breaking change (I think) -- had to do with passing messages as a list defined outside of the func call

--- app.py
+++ app.py
@@ -6,14 +6,15 @@ Description: This file contains the main Flask app that handles the autocomplete
 the app autocompletes the user's response as if this specific user experienced a specific event.
 """
 
-from config import AppConfig
 import json
-import string
 import random
 import re
-from flask import Flask, render_template, request, redirect, url_for, session, flash, jsonify
-from forms import CharacterForm, EventForm
+import string
 
+from flask import Flask, render_template, request, redirect, url_for, session, flash, jsonify
+
+from config import AppConfig
+from forms import CharacterForm, EventForm
 
 app = Flask(__name__)
 app_config = AppConfig()
@@ -33,9 +34,11 @@ def initial():
         session['event_description'] = app_config.event_description
         return redirect(url_for('index'))
 
+
 @app.route('/index')
 def index():
     return render_template('index.html', debounce_time=app_config.debounce_time, min_sentences=app_config.min_sentences)
+
 
 @app.route('/autocomplete', methods=['GET', 'POST'])
 def autocomplete():
@@ -43,26 +46,25 @@ def autocomplete():
     text = normalize_spacing(request.json.get('text'))
     context, incomplete_sentence = get_context_and_incomplete_sentence(normalize_spacing(text))
     context, incomplete_sentence = normalize_spacing(context), normalize_spacing(incomplete_sentence)
+    ct = random.random()
+    print(ct)
+    print(app_config.event_relevant)
     include_event = random.random() <= app_config.event_relevant
     print("include_event", include_event)
-    completion = normalize_spacing(get_chat_completion(
-        character_description=session['character_description'],
-        event=session['event_name'],
-        event_effects=session['event_description'],
-        include_event = include_event,
-        model=app_config.model,
-        context=context,
-        incomplete_sentence=incomplete_sentence,
-        temperature=random.uniform(*app_config.temperature_range),
-        max_tokens=random.randint(*app_config.token_range),
-        top_p=app_config.top_p)
-    )
+    completion = normalize_spacing(
+        get_chat_completion(character_description=session['character_description'], event=session['event_name'],
+                            event_effects=session['event_description'], include_event=include_event, model=app_config.model,
+                            context=context, incomplete_sentence=incomplete_sentence,
+                            temperature=random.uniform(*app_config.temperature_range),
+                            max_tokens=random.randint(*app_config.token_range), top_p=app_config.top_p))
     # Make sure LLM didn't stop mid-word
     full_word_completion = normalize_spacing(extract_complete_words(completion))
     de_duped_completion = normalize_spacing(remove_duplicated_completion(incomplete_sentence, full_word_completion))
-    d = {'text': text, 'context': context, 'incomplete_sentence': incomplete_sentence, 'completion': completion, 'full_word_completion': full_word_completion, 'de_duped_completion': de_duped_completion}
-    #print(d)
+    d = {'text': text, 'context': context, 'incomplete_sentence': incomplete_sentence, 'completion': completion,
+         'full_word_completion': full_word_completion, 'de_duped_completion': de_duped_completion}
+    # print(d)
     return jsonify(completion=de_duped_completion)
+
 
 ############################################################
 # HANDLE CHARACTER CREATION
@@ -84,28 +86,25 @@ def char_and_event():
 
     return render_template('char_and_event.html', character_form=character_form, event_form=event_form)
 
+
 def get_dynamic_effects(character_description, event_description, attempt_no=0, max_attempts=2):
-        if attempt_no > max_attempts:
-            return None
-        else:
-            try:
-                client = app_config.client
-                response = client.chat.completions.create(model="gpt-3.5-turbo",
-                                                          messages=[
-                                                              {"role": "system",
-                                                               "content": "You are a helpful, factual, and highly specific assistant."},
-                                                              {"role": "user",
-                                                               "content": f"""INSTRUCTIONS\nGiven a description of a person, return an enumerated list of the likely effects of {event_description} on this person. 
+    if attempt_no > max_attempts:
+        return None
+    else:
+        try:
+            client = app_config.client
+            response = client.chat.completions.create(model="gpt-3.5-turbo", messages=[
+                {"role": "system", "content": "You are a helpful, factual, and highly specific assistant."},
+                {"role": "user",
+                 "content": f"""INSTRUCTIONS\nGiven a description of a person, return an enumerated list of the likely effects of {event_description} on this person. 
                      Be very specific and very realistic. The effects can be related to any aspect of the person (their personality, demographics, hobbies, location etc.) but the effects must be realistic and specific. Do not exaggerate.
                     DESCRIPTION:
-                    {character_description}"""}],
-                                                          temperature=0.6, max_tokens=250, top_p=1)
-                answer = json.loads(response.choices[0].json())['message']['content']
-                return answer
-            except Exception as e:
-                print(e)
-                return get_dynamic_effects(character_description, event_description, attempt_no + 1, max_attempts)
-
+                    {character_description}"""}], temperature=0.6, max_tokens=250, top_p=1)
+            answer = json.loads(response.choices[0].json())['message']['content']
+            return answer
+        except Exception as e:
+            print(e)
+            return get_dynamic_effects(character_description, event_description, attempt_no + 1, max_attempts)
 
 
 def construct_character_description(form):
@@ -115,63 +114,32 @@ def construct_character_description(form):
 ############################################################
 # FUNCTIONS FOR PROCESSING TEXT
 ############################################################
-def get_chat_completion(character_description,
-                        event,
-                        event_effects,
-                        context,
-                        incomplete_sentence,
-                        model,
-                        temperature,
-                        max_tokens,
-                        top_p,
-                        include_event = True,
-                        attempt_no=0,
-                        max_attempts=2):
+def get_chat_completion(character_description, event, event_effects, context, incomplete_sentence, model, temperature,
+                        max_tokens, top_p, include_event, attempt_no=0, max_attempts=1):
     if attempt_no > max_attempts:
         return None
     else:
         try:
             client = app_config.client
             if include_event:
-                messages = [
-                    {
-                        "role": "system",
-                        "content": f"INSTRUCTIONS\nFinish a sentence in the style of a character who is affected by {event}.\nCHARACTER DESCRIPTION:\n{character_description}\nEVENT:\n{event}\nEVENT EFFECTS:\n{event_effects}.\n\nGiven the CONTEXT of what the character wrote, finish the INCOMPLETE SENTENCE to sound like the character who is affected by the event."
-                    },
-                    {
-                        "role": "user",
-                        "content": f"CONTEXT:{context}\n\nINCOMPLETE SENTENCE:{incomplete_sentence}"
-                    }
-                ],
+                system_instructions = f"INSTRUCTIONS\nFinish a sentence in the style of a character who is affected by {event}.\nCHARACTER DESCRIPTION:\n{character_description}\nEVENT:\n{event}\nEVENT EFFECTS:\n{event_effects}.\n\nGiven the CONTEXT of what the character wrote, finish the INCOMPLETE SENTENCE to sound like the character who is affected by the event."
             else:
-                messages = [
-                    {
-                        "role": "system",
-                        "content": f"INSTRUCTIONS\nFinish a sentence in the style of a character.\nCHARACTER DESCRIPTION:\n{character_description}.\n\nGiven the CONTEXT of what the character wrote, finish the INCOMPLETE SENTENCE to sound like the character, consistent with what was written."
-                    },
-                    {
-                        "role": "user",
-                        "content": f"CONTEXT:{context}\n\nINCOMPLETE SENTENCE:{incomplete_sentence}"
-                    }
-                ]
-            response = client.chat.completions.create(
-                model=model,
-                messages = messages,
-                temperature=temperature,
-                max_tokens=max_tokens,
-                top_p=top_p
-            )
+                system_instructions = f"INSTRUCTIONS\nFinish a sentence in the style of a character.\nCHARACTER DESCRIPTION:\n{character_description}.\n\nGiven the CONTEXT of what the character wrote, finish the INCOMPLETE SENTENCE to sound like the character, consistent with what was written."
+
+            response = client.chat.completions.create(model=model,
+                                                      messages=[{"role": "system", "content": system_instructions},
+                                                                {"role": "user", "content": f"CONTEXT:{context}\n\nINCOMPLETE SENTENCE:{incomplete_sentence}"}],
+                                                      temperature=temperature, max_tokens=max_tokens, top_p=top_p)
             answer = json.loads(response.choices[0].json())['message']['content']
-            for bad_word in ["INCOMPLETE SENTENCE", "CONTEXT", "INCOMPLETE SENTENCE", "CONTEXT"]:
-                if bad_word in answer:
-                    answer = answer.replace(bad_word+":", "")
-                    answer = answer.replace(bad_word + ":", "")
 
             return answer
         except Exception as e:
             print(e)
             return get_chat_completion(context=context, incomplete_sentence=incomplete_sentence, model=model,
+                                       temperature=temperature, max_tokens=max_tokens, top_p=top_p,
                                        include_event=include_event, attempt_no=attempt_no + 1, max_attempts=2)
+
+
 def remove_duplicated_completion(incomplete_sentence, completion):
     """
     Sometimes the LLM returns a completion that is a duplicate of the incomplete sentence. This function removes that duplication.
@@ -261,9 +229,17 @@ def get_context_and_incomplete_sentence(text):
 def extract_complete_words(text):
     """
     Extracts complete words from the given text since sometimes the LLM returns
-    incomplete words. In practice, it is expensive to accurately check if a word is complete (would require spell-check or something)
-    so a simple heuristic is used: if the word ends with a space or punctuation, it is complete. Else, assume it could be
-    incomplete and remove that word.
+    incomplete words depending on tokens. But in practice, it is expensive to accurately check if a word is complete,
+    so a simple heuristic is used:
+
+    - If the immediate character before the last word is whitespace or punctuation, then the word is neceesarily complete.
+    - If the word is not necessarily complete, then it is assumed to be incomplete.
+    - Note: This means in practice we often just delete the last word of the completion.
+
+    EXAMPLES
+    "I went to the store to buy some mil" -> "I went to the store to buy some"
+    "I went to the store to buy some milk" -> "I went to the store to buy some"
+
     """
     if not text:
         return text
