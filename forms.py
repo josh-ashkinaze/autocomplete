dc00d798c608f5e93273dd0bd6f8aa8a795762af
Joshua Ashkinaze <josh.ashkinaze@gmail.com>
2023-12-26 20:41:09 -0500

    update copy

--- forms.py
+++ forms.py
@@ -9,13 +9,13 @@ class CharacterForm(FlaskForm):
     occupation = StringField('Occupation', validators=[DataRequired(), Length(max=100)],
                              render_kw={"placeholder": "What do you do for work? Ex: Late-stage mafia boss"})
     location = StringField('Location', validators=[DataRequired(), Length(max=100)],
-                           render_kw={"placeholder": "What region or city are you living in? Ex: Iowa"})
+                           render_kw={"placeholder": "What region or city are you living in? Ex: the fun part of Iowa"})
     hobbies = TextAreaField('Hobbies', validators=[DataRequired()],
-                            render_kw={"placeholder": "What do you like to do? List some activities. Ex: Origami"})
+                            render_kw={"placeholder": "What do you like to do? List some activities. Ex: origami, paint, meditate"})
     personality = TextAreaField('Personality', validators=[DataRequired(), Length(max=200)], render_kw={
         "placeholder": "How would you describe yourself? For example, list three adjectives or what you think would be on your tombstone."})
 
 
 class EventForm(FlaskForm):
     event = TextAreaField('Event', validators=[DataRequired(), Length(max=100)], render_kw={
-        "placeholder": "What is the event that should happen? Ex: Unchecked climate change, winning the lottery, losing a lottery ticket, etc."})
+        "placeholder": "What is the event that should happen? Ex: unchecked climate change, winning the lottery, losing a lottery ticket"})
